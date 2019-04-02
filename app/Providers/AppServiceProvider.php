30470918b1d9d56faab33f10fd81b5d437d62d43
Jon Ouellette <jonaouellette@gmail.com>
2019-04-02 02:45:33 -0700

    Added default length.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
         \View::composer('posts.create', function ($view) {
             $categories = \Cache::rememberForever('categories', function () {
                 return Category::all();
